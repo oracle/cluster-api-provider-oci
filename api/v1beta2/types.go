6ca4a9c504daa6af639da0da41b143ea75ef34b6
Shyam Radhakrishnan <shyam.radhakrishnan@oracle.com>
2023-06-28 10:54:40 +0530

    Add Subnet Id and NSG ID to machine spec

--- api/v1beta2/types.go
+++ api/v1beta2/types.go
@@ -34,8 +34,7 @@ var OCIManagedClusterSubnetRoles = []Role{PodRole, ControlPlaneEndpointRole, Wor
 
 // NetworkDetails defines the configuration options for the network
 type NetworkDetails struct {
-	// SubnetId defines the ID of the subnet to use.
-	// Deprecated, use SubnetName parameter
+	// SubnetId defines the ID of the subnet to use. This parameter takes priority over SubnetName.
 	SubnetId *string `json:"subnetId,omitempty"`
 
 	// AssignPublicIp defines whether the instance should have a public IP address
@@ -47,7 +46,7 @@ type NetworkDetails struct {
 	// SkipSourceDestCheck defines whether the source/destination check is disabled on the VNIC.
 	SkipSourceDestCheck *bool `json:"skipSourceDestCheck,omitempty"`
 
-	// Deprecated, use 	NsgNames parameter to define the NSGs
+	// NSGId defines the ID of the NSG to use. This parameter takes priority over NsgNames.
 	NSGId *string `json:"nsgId,omitempty"`
 
 	// NsgNames defines a list of the nsg names of the network security groups (NSGs) to add the VNIC to.
