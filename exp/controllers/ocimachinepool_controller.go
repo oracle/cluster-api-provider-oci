340fa9d026745601c91a274959c12f0e26e49dcb
Shyam Radhakrishnan <shyam.radhakrishnan@oracle.com>
2023-08-23 20:00:21 +0530

    Fix machinepool infrastructure cluster lookup

--- exp/controllers/ocimachinepool_controller.go
+++ exp/controllers/ocimachinepool_controller.go
@@ -19,7 +19,6 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -48,6 +47,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
