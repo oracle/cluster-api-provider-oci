311628c4a081be66a9ef3a3daac0f5a0c2cc2922
Shyam Radhakrishnan <shyam.radhakrishnan@oracle.com>
2024-03-14 16:28:13 +0530

    Add support for multiple NSGs in machine spec

--- api/v1beta2/zz_generated.deepcopy.go
+++ api/v1beta2/zz_generated.deepcopy.go
@@ -1153,6 +1153,11 @@ func (in *NetworkDetails) DeepCopyInto(out *NetworkDetails) {
 		*out = new(string)
 		**out = **in
 	}
+	if in.NSGIds != nil {
+		in, out := &in.NSGIds, &out.NSGIds
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	if in.NsgNames != nil {
 		in, out := &in.NsgNames, &out.NsgNames
 		*out = make([]string, len(*in))
